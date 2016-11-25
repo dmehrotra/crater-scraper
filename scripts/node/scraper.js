a329ebf8967af76f852e60bab056b11b2644d2dd
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-11-24 21:01:00 -0500

    initial scraper and cropper

---
+++ scripts/node/scraper.js
@@ -0,0 +1,27 @@
+var GoogleMapsAPI = require('./node_modules/googlemaps/lib/index');
+var fs = require('fs')
+var google = new GoogleMapsAPI();
+var cropper= require('easyimage')
+var params = {
+  zoom: 15,
+  size: '500x400',
+  maptype: 'satellite',
+  center:"37.14228  -116.04911"
+};
+
+
+google.staticMap(params); // return static map URL
+google.staticMap(params, function(err, binaryImage) {
+  fs.writeFile('this.jpg', binaryImage, 'binary', function(err){
+    if (err) throw err   
+    cropper.rescrop({
+         src:'this.jpg', dst:'./this.jpg',
+         width:500, height:400,
+         cropwidth:500, cropheight:350,
+      }).then(
+      function (err) {
+        console.log(err);
+      }
+    );
+  })
+});
