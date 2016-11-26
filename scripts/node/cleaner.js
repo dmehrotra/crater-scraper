2e828d4db401eb1b9aed8f4f87e57fdaba6cbf9e
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-11-26 16:23:11 -0500

    images

--- scripts/node/cleaner.js
+++ scripts/node/cleaner.js
@@ -1,1 +1,57 @@
-cleaner.js
+'use strict';
+var _ = require('underscore');
+var csv = require('fast-csv')
+
+function Cleaner(){
+	this.description = "Cleaner for parsing the csv";
+	this.raw = [
+		"./data/US-1945-1963.csv",
+		"./data/US-1964-1972.csv",
+		"./data/US-1973-1992.csv",
+		"./data/USSR-1964-1990.csv",
+		"./data/UK_FRANCE_CHINA-1964-1993.csv"
+		]
+	this.sanitized_craters =[];
+	this.clean()
+
+}
+Cleaner.prototype.clean = function(){
+	var self = this;
+	_.each(this.raw,function(v,i){
+		prepare(v,function(obj){
+			_.each(obj,function(v,i){
+				self.sanitized_craters.push(v)
+			})
+		})
+	})
+}
+function prepare(raw,callback){
+	var c = [];
+	csv.fromPath(raw).on("data", function(data){
+    	
+    	var obj = {
+    		"SERIES" : data[0],
+    		"SHOT" : data[1],
+    		"YEAR" : data[2],
+    		"MON" : data[3],
+    		"LAT" : data[4],
+    		"LONG" : data[5],
+    		"HOB" : data[6],
+    		"TYPE" : data[7],
+    		"PUR" : data[8],
+    		"CRAT" : data[9],
+    		"DEVIC" : data[10],
+    		"WARHEAD" : data[11]
+    	}
+    	c.push(obj)
+ 	})
+ 	.on("end", function(){
+    	callback(c)
+ 	});
+}
+module.exports = {
+	begin: function(){
+		return new Cleaner()	
+		}
+	}
+
--- scripts/node/scraper.js
+++ scripts/node/scraper.js
@@ -1,28 +1,42 @@
 var GoogleMapsAPI = require('./node_modules/googlemaps/lib/index');
 var fs = require('fs')
 var google = new GoogleMapsAPI();
-var cropper= require('easyimage')
-var params = {
-  zoom: 15,
-  size: '500x400',
-  maptype: 'satellite',
-  center:"37.14228  -116.04911"
-};
+var cropper = require('easyimage')
+var cleaner = require('./cleaner.js')
+var _ = require('underscore')
+EXITCONDITION = false
 
 
-google.staticMap(params); // return static map URL
-google.staticMap(params, function(err, binaryImage) {
-  fs.writeFile('this.jpg', binaryImage, 'binary', function(err){
-    if (err) throw err   
-    cropper.rescrop({
-         src:'this.jpg', dst:'./this.jpg',
-         width:500, height:400,
-         cropwidth:500, cropheight:350,
-      }).then(
-      function (err) {
-        console.log(err);
-      }
-    );
-  })
-});
+
  
+var sites = cleaner.begin()
+
+setTimeout(function() { run(sites.sanitized_craters)}, 10000)
+
+function run(data){
+  var d = data;
+ console.log('su')
+  _.each(d,function(v,k){
+      var params = {
+        zoom: 15,
+        size: '500x400',
+        maptype: 'satellite',
+        center:v['LAT'] + " " + v["LONG"]
+      };
+      google.staticMap(params, function(err, binaryImage) {
+        
+        fs.writeFile(v['SERIES']+v['YEAR']+k, binaryImage, 'binary', function(err){
+          if (err) throw err   
+          cropper.rescrop({
+               src:v['SERIES']+v['YEAR']+k, dst:v['SERIES']+v['YEAR']+k,
+               width:500, height:400,
+               cropwidth:500, cropheight:350,
+            }).then(
+            function (err) {
+              console.log(err);
+            }
+          );
+        })
+      });
+  })
+}
